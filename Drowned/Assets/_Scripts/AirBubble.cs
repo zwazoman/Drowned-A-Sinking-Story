cfd1ca092713151b4dfc45d93ac2cf380fb4ee34
zwazoman <141416442+zwazoman@users.noreply.github.com>
2025-01-25 19:04:32 +0100

    fjaikf

--- Drowned/Assets/_Scripts/AirBubble.cs
+++ Drowned/Assets/_Scripts/AirBubble.cs
@@ -2,9 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AirBubble : MonoBehaviour
+public class AirBubble : MonoBehaviour,IPoolable
 {
     [SerializeField] float _airAmount;
+
+    PoolObject _poolObject;
+
+    private void Awake()
+    {
+        _poolObject.OnPulledFromPool += OnPulledFromPool;
+
+        TryGetComponent( out _poolObject);
+    }
+
+    public void OnPulledFromPool()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ReturnToPool()
+    {
+        if (_poolObject == null) Destroy(gameObject); else _poolObject.PushToPool();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
