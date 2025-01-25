0dccaf97007fd808943cb4c063d43c8b55f42ffa
nathan-exe <nathan.tazi.1@gmail.com>
2025-01-25 16:35:52 +0100

    debut vfx bulles

--- Drowned/Assets/BodyPart.cs
+++ Drowned/Assets/BodyPart.cs
@@ -19,7 +19,7 @@ public class BodyPart : MonoBehaviour
         if(Time.time - FlipFlop.TimeSinceFlipFlop < .2f)
         {
             rb.AddForce(Vector3.ProjectOnPlane( transform.up,Vector3.up)*ForwardBonusForce,ForceMode.Impulse);
-            Debug.DrawRay(transform.position,Vector3.ProjectOnPlane(transform.up, Vector3.up) * ForwardBonusForce, Color.red,1f);
+            //Debug.DrawRay(transform.position,Vector3.ProjectOnPlane(transform.up, Vector3.up) * ForwardBonusForce, Color.red,1f);
             print("bonus");
         }
     }
