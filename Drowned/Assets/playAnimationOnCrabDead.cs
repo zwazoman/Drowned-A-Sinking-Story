8c9ae9f9219a03dc701d99cbf7cb24f5a51edd0e
nathan-exe <nathan.tazi.1@gmail.com>
2025-01-26 14:29:15 +0100

    qfs

--- Drowned/Assets/playAnimationOnCrabDead.cs
+++ Drowned/Assets/playAnimationOnCrabDead.cs
@@ -7,6 +7,7 @@ public class playAnimationOnCrabDead : MonoBehaviour
     
     void Start()
     {
+        GetComponent<Animation>().Stop();
         BossHealth.OnBossDead += ()=> GetComponent<Animation>().Play();
     }
 
