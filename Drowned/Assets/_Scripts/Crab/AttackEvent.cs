2155444352aede7eae033f165c983be5092392de
zwazoman <141416442+zwazoman@users.noreply.github.com>
2025-01-29 13:18:25 +0100

    yfuuyg

--- Drowned/Assets/_Scripts/Crab/AttackEvent.cs
+++ Drowned/Assets/_Scripts/Crab/AttackEvent.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AttackEvent : MonoBehaviour
 {
+    [SerializeField] Transform _shootSocket;
+    [SerializeField] Transform _shootSocketlautre;
+
     [SerializeField] List<CrabClaws> _claws = new List<CrabClaws>();
 
     public void Yes()
@@ -20,5 +23,18 @@ public class AttackEvent : MonoBehaviour
         {
             claw.StopAttack();
         }
+    }
+
+    public void ShootBubbles()
+    {
+        Vector3 target = FishController.Instance.gameObject.transform.position;
+        Vector3 clawToTarget = target - _shootSocket.position;
+        GameObject bubble = PoolManager.Instance.AccessPool(Pools.CrabBubble).TakeFromPool(-_shootSocket.position, Quaternion.identity);
+        bubble.transform.forward = clawToTarget;
+    }
+
+    public void ShootBigRayonLaserDeLaMortHIHI()
+    {
+
     }
 }
--- Drowned/Assets/_Scripts/Pools/PoolManager.cs
+++ Drowned/Assets/_Scripts/Pools/PoolManager.cs
@@ -5,6 +5,7 @@ public enum Pools
 {
     Bubble,
     AirBubble,
+    CrabBubble
 }
 
 /// <summary>
